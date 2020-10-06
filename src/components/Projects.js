dc22b58cda4a690d537bd3fe75bc8a18a5109c3f
Jesus Arciniega <dev.jesusarc@gmail.com>
2020-10-06 01:10:56 -0600

    Read
    Get values from Firestore collection

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ProjectForm from "./ProjectForm";
 
 import {db} from '../firebase';
 
 const Projects = () => {
 
+    const [projects, setProjects] = useState([]);
+
     //Function to save project data.
     const addOrEditProject = async projectData => {
         await db.collection('projects').doc().set(projectData);
-        console.log('Project Created');
     }
+
+    //Load the projects on state changes.
+    const getProjects = async () => {
+        db.collection('projects').onSnapshot(snap => {
+            const docs = [];
+            snap.forEach(project => {
+                docs.push({ ...project.data(), id: project.id});
+            });
+            setProjects(docs);
+        });
+    }
+
+    useEffect(() => {
+        getProjects();
+    }, []);
 
     return (
         <div>
             <ProjectForm addOrEditProject={addOrEditProject} />
-            <h1>Projects</h1>
+            <div className="col s12">
+                {projects.map(project => {
+                    if (project.project_name){
+                        return (
+                            <div className="card">
+                                <div class="col s12">
+                                    <div class="card blue-grey darken-1">
+                                        <div class="card-content white-text">
+                                            <span class="card-title">{project.project_name}</span>
+                                            <p>{project.project_description}</p>
+                                        </div>
+                                        <div class="card-action">
+                                            <a href={project.project_url}>Project URL</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
+            </div>
         </div>
     )
 }
