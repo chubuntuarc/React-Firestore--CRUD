8dbbe8fc43c999593fb5343663a34ec44bd602c8
Jesus Arciniega <dev.jesusarc@gmail.com>
2020-10-06 01:20:30 -0600

    Delete
    Delete a document from projects collection

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,13 @@ const Projects = () => {
         await db.collection('projects').doc().set(projectData);
     }
 
+    //Function to delete projects.
+    const onDeleteProject = async id => {
+        if (window.confirm('Are you sure to delete the project?')){
+            await db.collection('projects').doc(id).delete();
+        }
+    }
+
     //Load the projects on state changes.
     const getProjects = async () => {
         db.collection('projects').onSnapshot(snap => {
@@ -34,15 +41,15 @@ const Projects = () => {
                 {projects.map(project => {
                     if (project.project_name){
                         return (
-                            <div className="card">
-                                <div class="col s12">
-                                    <div class="card blue-grey darken-1">
-                                        <div class="card-content white-text">
-                                            <span class="card-title">{project.project_name}</span>
+                            <div className="card" key={project.id}>
+                                <div className="col s12">
+                                    <div className="card blue-grey darken-1">
+                                        <div className="card-content white-text">
+                                            <span className="card-title">{project.project_name} <i className="material-icons right red-text" onClick={() => onDeleteProject(project.id)}>delete</i></span>
                                             <p>{project.project_description}</p>
                                         </div>
-                                        <div class="card-action">
-                                            <a href={project.project_url}>Project URL</a>
+                                        <div className="card-action">
+                                            <a href={project.project_url} target="_blank" rel="noopener noreferrer">Project URL</a>
                                         </div>
                                     </div>
                                 </div>
